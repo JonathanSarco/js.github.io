856dd0da375ada5dac9d1a964b8a5e6f42d04008
Jony Sarco <jfe.sarco@gmail.com>
2022-10-02 18:48:00 +0200

    change title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
+  title: "JS",
   tagline: "Dinosaurs are cool",
   url: "https://github.com",
   baseUrl: "/js.github.io/",
